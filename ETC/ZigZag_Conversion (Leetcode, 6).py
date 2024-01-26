b5a9a2eba074ea04823f4003607cd198174f09f5
dohyun96305 <dohyun96305@naver.com>
2024-01-26 12:06:49 +0900

    240125_Coding

---
+++ ETC/ZigZag_Conversion (Leetcode, 6).py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/zigzag-conversion/description/
+# numRows 값에 따라 문자열 S를 지그재그로 변환 및 반환
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        temp = [''] * numRows # numRows 값에 따라 빈 문자열 및 List 생성 
+
+        index = 0
+
+        if numRows == 1 or numRows >= len(s) : 
+            return s
+
+        else : 
+            for i in s : # Time Complexity : O(n)
+                up = 1
+                down = -1
+                temp[index] += i # numRows 및 Index에 따라 temp List에 문자열 하나씩 저장
+
+                if index == 0 : # 0 ~ numRows-1 까지는 1씩 증가
+                    move = up
+                elif index == numRows -1 : # numRows-1 ~ 0까지는 1씩 감소
+                    move = down
+
+                index = index + move # Index 값에 따른 move 증가
+
+            return ''.join(temp) # 조건문 완료 후 temp 내 문자열 붙여서 반환
+                
+
+# ((3))
+# 0 1 2 3 4 5 6 7 8
+# 0 1 2 1 0 1 2 1 0
+
+# ((4))
+# 0 1 2 3 4 5 6 7 8 9 10 11 12
+# 0 1 2 3 2 1 0 1 2 3 2  1  0
---
+++ HashTable/Two_Sum_(Leetcode, 1).py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/two-sum/description/
+# List 내 2개 원소의 합이 Target 값이라면 Index 값 반환
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        temp = {} # HashTable 생성, 값 : Index 저장
+
+        for i, k in enumerate(nums) : # Time Complexity : O(n)
+            
+            diff = target - k # 반복문을 통해 조건 확인
+
+            if diff in temp : # 조건을 만족하는 값이 HashTable에 있다면 Index 값 반환
+                return [temp[diff], i]
+
+            temp[k] = i # 조건을 만족하지 못한다면 HashTable 값 저장 
+
+
+            
+        
